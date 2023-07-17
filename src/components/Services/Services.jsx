af4dc6407a2d91caa9f800d4bdfde45894723434
abhishek1stgit <abhivishw2002@gmail.com>
2023-07-17 11:21:10 +0530

    after ad build code

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -32,7 +32,7 @@ const Services = () => {
           passionate about creating user-friendly websites
         </spane>
         <a href={Resume} download>
-          <button className="button s-button">Download CV</button>
+          <button className="button s-button">Download(CV)</button>
         </a>
         <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
       </div>
